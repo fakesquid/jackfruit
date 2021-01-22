870d3324eefc24d5c65208d6771a37f522e0d22d
fakesquid <zjzhang1@gmail.com>
2021-01-22 16:19:51 -0500

    fixed grammar

--- src/pages/work/shenkii.js
+++ src/pages/work/shenkii.js
@@ -65,12 +65,12 @@ const Blockchain = () => {
           problem by assigning blockchain certificates to artworks.
           <br />
           <br />
-          With a blockchain certificate, authenticity can be verified, origin can be tracked, and
-          ownership can be proved.
+          With a blockchain certificate, authenticity can be verified, the origin can be tracked,
+          and ownership can be proved.
           <br />
           <br />
-          However, most blockchain art marketplaces were generic, lacked control for artist, and
-          lacked the user experience for tradition Web 2.0.
+          However, most blockchain art marketplaces were generic, lacked control for the artist, and
+          lacked the user experience for traditional Web 2.0.
         </p>
       </div>
     </div>
@@ -84,7 +84,7 @@ const TheSolution = () => {
       <div className="case-text">
         <p>
           A blockchain art marketplace for Japanese-styled art, manga, and doujinshi. The platform
-          will be design with a focus on giving the artist the control, and to provide a familiar
+          will be designed with a focus on giving the artist the control and providing a familiar
           Web 2.0 experience to allow ease of usage and onboarding for users.
         </p>
       </div>
@@ -108,7 +108,7 @@ const Gains = () => {
             work
           </li>
           <br />
-          <li>Provide a social space fo r users, artists, and collectors to share and view art</li>
+          <li>Provide a social space for users, artists, and collectors to share and view art</li>
         </ol>
       </div>
     </div>
@@ -128,13 +128,13 @@ const UserPersonas = () => {
           <div className="user-details">
             <h5>Behavior</h5>
             <p>
-              Jun Takahashi is an amateur freelance manga artist from Tokyo, Japan. Jun have 5 years
+              Jun Takahashi is an amateur freelance manga artist from Tokyo, Japan. Jun has 5 years
               of experience drawing manga and sell his doujinshi on various online platforms.
             </p>
             <h5>Obstacles</h5>
             <p>
-              Jun’s biggest concerns with current online platform are the lack of profitability and
-              protection of copyright.
+              Jun’s biggest concerns with the current online platform are the lack of profitability
+              and protection of copyright.
             </p>
             <h5>Gains</h5>
             <p>
@@ -153,7 +153,7 @@ const UserPersonas = () => {
           <div className="user-details">
             <h5>Behavior</h5>
             <p>
-              Fan Kang is an university student from Toronto, Canada. An avid manga reader, Fan is
+              Fan Kang is a university student from Toronto, Canada. An avid manga reader, Fan is
               passionate about Japanese culture and often collect both physical and digital
               merchandise from his favorite manga and anime.
             </p>
@@ -185,18 +185,18 @@ const Userflow = () => {
           From our previous identification of the problems that persist amongst current blockchain
           marketplaces, the user experience and usability of new users to the blockchain space was a
           key problem. With so many centralized social media and web platforms closely integrated
-          with our daily lives, it may not be obvious to first time users what it means to be a
+          with our daily lives, it may not be obvious to first-time users what it means to be a
           decentralized platform, or what values it offers. Many users may be unfamiliar with the
-          term "wallet" in Web 3.0 context, and may be wondering why there isn't a way for them to
-          create an account.
+          term "wallet" in the Web 3.0 context and may be wondering why there isn't a way for them
+          to create an account.
         </p>
         <br />
         <p>
-          These are valid confusions and frustrations that the user would have, if it's their first
-          time interacting with a decentralized platform built on top of blockchain. That's why when
-          I designed the user flow of our platform, I took careful consideration into how I can make
-          the onboarding experience for new users better. I took experience from our team's first
-          time with blockchain, and build the guidance that I wish were there at the time.
+          These are valid confusions and frustrations that the user would have if it's their first
+          time interacting with a decentralized platform built on top of the blockchain. That's why
+          when I designed the user flow of our platform, I took careful consideration into how I can
+          make the onboarding experience for new users better. I took the experience from our team's
+          first time with blockchain, and build the guidance that I wish were there at the time.
         </p>
         <br />
         <h3>Key designs of the user flow</h3>
@@ -204,7 +204,7 @@ const Userflow = () => {
           <li>Build an experience that the users are familiar with</li>
           <li>Choose technologies that minimize the barrier of entry and learning</li>
           <li>
-            Make the a comprehensive and informational About page that will educate the users on the
+            Make a comprehensive and informational About page that will educate the users on the
             technology, and provide answers to FAQ
           </li>
           <li>
@@ -233,8 +233,8 @@ const Marketplace = () => {
         <p>
           Different from traditional e-commerce marketplaces such as Amazon, a marketplace for art
           should be a visual-first exploratory experience. I wanted the user to experience the
-          feelings of going to an art gallery, not a marketplace. Instead of cluttering the user's
-          view with competing prices and endless options, I wanted the users have their focus on
+          feeling of going to an art gallery, not a marketplace. Instead of cluttering the user's
+          view with competing prices and endless options, I wanted the users to have their focus on
           appreciating the art.
         </p>
         <br />
@@ -242,7 +242,7 @@ const Marketplace = () => {
           I went with a very minimalistic design with our marketplace. Decluttering the UIs to free
           up as much space as possible to spotlight each and every piece of art. The card UI
           containing the art was designed to maximize the viewing size of the art, taking
-          inspirations from art galleries.
+          inspiration from art galleries.
         </p>
       </div>
     </div>
@@ -259,13 +259,13 @@ const ArtPage = () => {
         <ShenkiiImage id={7} />
         <h5 className="helper-text">Past iterations of the Art page</h5>
         <p>
-          With the art page, I continue to design with the philosophy that when building a online
-          marketplace for digital art, the art should be the focus of the user's attention. Like the
-          real world experience of walking up to a piece of artwork that you find intriguing in the
-          gallery, I wanted the user to have a similar experience when selecting into a specific
-          piece of artwork. In the art page, the user is able to see the artwork at a much greater
-          detail and size. More relevant information such as detailed description, number of copies,
-          past ownership will also be displayed.
+          With the art page, I continue to design with the philosophy that when building an online
+          marketplace for digital art, art should be the focus of the user's attention. Like the
+          real-world experience of walking up to a piece of artwork that you find intriguing in the
+          gallery, I wanted the user to have a similar experience when selecting a specific piece of
+          artwork. On the art page, the user is able to see the artwork in much greater detail and
+          size. More relevant information such as detailed description, number of copies, past
+          ownership will also be displayed.
         </p>
         <br />
         <p>
@@ -286,16 +286,16 @@ const NextSteps = () => {
       <div className="case-text">
         <p>
           As Shenkii is currently still in active design and development, I anticipate the platform
-          to continue to evolve with more feedbacks and research. Most of the decisions made with
-          the design were based off of the insights that we have gathered through speaking with our
-          users and researching the current market. While our team's current focus is on implement
-          the backend, I aim to conduct further usability tests my design decisions.
+          to continue to evolve with more feedback and research. Most of the decisions made with the
+          design were based on the insights that we have gathered through speaking with our users
+          and researching the current market. While our team's current focus is on implement the
+          backend, I aim to conduct further usability tests on my design decisions.
         </p>
         <br />
         <p>
           Learning from our market and user research, another of our key focus is on the artist. We
           believe in delivering better tools to help artists in the tokenization, protection, and
-          sale of their artworks. Expect design of features and tools for artist coming soon.
+          sale of their artworks. Expect the design of features and tools for artists coming soon.
         </p>
       </div>
     </div>
@@ -313,7 +313,7 @@ const Learnings = () => {
           While it was the first time for our team to develop with blockchain technologies, it was
           also my first time in taking a lead role in product design. With those two factors
           combined, I faced a lot of unknowns and technical constraints when designing Shenkii. One
-          of the biggest challenge that I continuously work on with design is how to make the dApp
+          of the biggest challenges that I continuously work on with design is how to make the dApp
           experience for new users feel familiar and frictionless.
         </p>
       </div>
