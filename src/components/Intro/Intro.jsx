966c60bab03696a429b7580bfc082d12b1849413
fakesquid <zjzhang1@gmail.com>
2021-01-22 17:21:47 -0500

    updated intro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -4,14 +4,36 @@ const Headline = () => {
   return (
     <div className="container">
       <h1 className="main-headline">
-        <span className="hello">Hello.</span>
-        <br />
-        <span>
-          I'm Jack, a <span className="bold1">UX Developer</span> and{' '}
-          <span className="bold2">Product Designer</span> primarily working on UI/UX interfaces. I
-          geek out on everything design related, currently obsessed with typography and shoes.
-        </span>
+        <span className="hello">Hey I'm Jack!</span>
+        <br />A <span className="bold1">UX Developer</span> and{' '}
+        <span className="bold2">Product Designer</span> primarily working on UI/UX interfaces.
+        Through design, I want to make the world a better place.
       </h1>
+    </div>
+  );
+};
+
+const Other = () => {
+  return (
+    <div className="container">
+      <div className="others-banner">
+        <ul>
+          <span>Previously At:</span>
+          <li>Autodesk</li>
+          <li>Hubba</li>
+          <li>Flashfood</li>
+          <li>TD</li>
+          <li>NUS</li>
+        </ul>
+        <ul>
+          <span>Hackathons:</span>
+          <li>ETHGlobal MarketMake</li>
+          <li>Hack the North 2019</li>
+          <li>Hack the North 2018</li>
+          <li>Hack the North 2017</li>
+          <li>StarterHack 2017</li>
+        </ul>
+      </div>
     </div>
   );
 };
