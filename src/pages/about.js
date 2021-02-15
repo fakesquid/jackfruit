677c9c25885f866c39ed37eedbc1efa02ef13ff9
fakesquid <zjzhang1@gmail.com>
2021-02-14 21:59:04 -0500

    updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import AboutImage from '../components/Image/AboutImg';
+import Footer from '../components/Footer/Footer';
 
 import '../style/main.scss';
 
@@ -13,17 +14,36 @@ const About = () => {
           <div className="about-details">
             <h1>Aim for the moon, and shoot for the stars.</h1>
             <p>
-              Just when I thought that biotech would become my calling, my tendency to stay curious
-              led to the world of product design and software development.
+              Hey there! I’m Jack, a self-taught UX developer and product designer studying biotech
+              at the University of Waterloo. Previously, I've interned at Autodesk, Hubba,
+              Flashfood, TD, and NUS in product design and development.{' '}
+              <span className="details-bold">
+                Currently, I am looking for software development and product design opportunities!
+              </span>
             </p>
             <p>
-              I’m Jack, a graduating student studying biotechnology at the University of Waterloo.
-              I’m a self-taught UX developer and product designer, currently designing for NFPs at
-              Blueprint.
+              I'm passionate about solving problems with technology to make the world a better
+              place. Initially, I pursued biotech because I was intrigued by mycelium's potential to
+              replace plastics. However, my curiosity in technology led me to discover product
+              design and software development through various hackathons.
+            </p>
+            <p>
+              I care deeply about making a positive social impact and take every opportunity that I
+              can to do so. Since 2019, I've been volunteering for{' '}
+              <a
+                className="others-title"
+                href="https://uwblueprint.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                UW Blueprint
+              </a>
+              , a student-led team that builds technology for nonprofits.
             </p>
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 };
